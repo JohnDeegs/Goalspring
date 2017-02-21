2d87fe78d40fc69226ad77e6b2268c4193ebcaae
John Deegan <john.deegan@hotmail.com>
2017-02-21 14:56:25 +0000

    update for heroku

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var app      = express();
-const port     = process.env.PORT || 8080;
+var port     = Number(process.env.PORT || 8080);
 var passport = require('passport');
 var flash    = require('connect-flash');
 
