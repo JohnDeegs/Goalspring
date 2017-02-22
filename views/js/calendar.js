395f3a5faa1e5d5b547d5e28be61f731ef8e1c96
John Deegan <john.deegan@hotmail.com>
2017-02-22 03:04:14 +0000

    profile form mod

--- views/js/calendar.js
+++ views/js/calendar.js
@@ -63,12 +63,17 @@ $(document).ready(function () {
     });
   });
 
-  function submitTask(e){
+  $(function() {
+    $("#datepicker").datepicker({
+      onSelect: function(){
+        var dateObject = $(this).datepicker('getDate');
+        alert(dateObject);
+      }
+    });
+  });
 
-  }
-
-
-
+  $('#text2').timepicker();
+  $('#text3').timepicker();
 
   //Get user's current data if exists
   $.get("http://localhost:8080/tasks", function(data, status){
@@ -102,9 +107,20 @@ $(document).ready(function () {
     //var randomColor = colors[Math.floor(Math.random() * colors.length)];
 
     var text = $('#text1').val();
-    var type = $('#text2').val();
+    var sTime = $('#text2').val();
+    var fTime = $('#text3').val();
 
-    $('#taskList').append('<div id="taskBlock" style="background-color: '+colors[randomColor]+';"><p>'+text+'</p><p id="smallertext">13:00 - 14:00</p></div>').show();
+    var sCheck = sTime.charAt(0);
+    var fCheck = fTime.charAt(0);
+
+    sCheck = parseInt(sCheck);
+    fCheck = parseInt(fCheck);
+
+    if(sCheck > fCheck){
+      alert("There's no flux capacitor to take you back in time here!");
+    }else{
+    $('#taskList').append('<div id="taskBlock" style="background-color: '+colors[randomColor]+';"><p>'+text+'</p><p id="smallertext">'+sTime+' - '+fTime+'</p></div>').show();
+    }
 
     //$('#taskFormat').append('<div id="whitespace"></div>');
   });
@@ -113,51 +129,5 @@ $(document).ready(function () {
     $('#userSchedule tr:last').remove();
   });
 
-  $analyzeBtn.click(function(){
-    var studyTotal = 0;
-    var excerciseTotal = 0;
-    var socialTotal = 0;
-
-    for(var i = 0; i < studyArray.length; i++){
-      studyTotal += studyArray[i];
-    }
-
-    for(var i = 0; i < excerciseArray.length; i++){
-      excerciseTotal += excerciseArray[i];
-    }
-
-    for(var i = 0; i < socialArray.length; i++){
-      socialTotal += socialArray[i];
-    }
-
-    if(studyArray.length == 0 && excerciseArray.length == 0 && socialArray.length == 0){
-      alert("Your schedule is empty!");
-    }
-
-    if(studyArray.length == 0 || studyTotal < 3){
-      $studyComments.html('<li>You are not doing enough Study today. Please consider adding some productive activities to the list.</li><li>Most students at High School level study for up to 3 hours per day.</li><li>College and third level students reccomend up to 5 hours per day.</li>');
-    }else if(studyTotal > 8){
-      $studyComments.html('<li>Be careful! You are studying too much today, we reccomend that you slow down unless its urgent!</li><li>According to <a href="https://www.reference.com/health/harmful-effects-studying-much-afd5e4cbd8304901">reference.com</a>, too much studying can result in elevated stress and social alienation</li>');
-    }else{
-      $studyComments.html('<li style="list-style: none;"><i class="fa fa-check fa-5x" aria-hidden="true"></i></li>');
-    }
-
-    if(excerciseArray.length == 0 || excerciseTotal <= 1){
-      $excerciseComments.html('<li>You are not doing enough Excercise today. Please consider adding some active activities to the list.</li><li>According to the <a href="https://www.hsph.harvard.edu/nutritionsource/2013/11/20/physical-activity-guidelines-how-much-exercise-do-you-need/">Harvard School of Public Health</a>, the average adult should be excercising for 150 minutes per week.</li>');
-    }else if(excerciseTotal > 5){
-      $excerciseComments.html('<li>You are doing a lot of Excercise. Consider taking it easy tomorrow!</li><li>According to <a href="http://www.webmd.com/men/features/exercise-addiction#1">webmd.com</a>, "Too much exercise can lead to injuries, exhaustion, depression, and suicide. It can also cause lasting physical harm."</li>');
-    }else{
-      $excerciseComments.html('<li style="list-style: none;"><i class="fa fa-check fa-5x" aria-hidden="true"></i></li>');
-    }
-
-    if(socialArray.length == 0 || socialTotal <= 1){
-      $socialComments.html('<li>You are doing enough Socializing today. Please consider assing some social activities to the list.</li>According to <a href="http://ie.reachout.com/getting-help/face-to-face-help/things-you-need-to-know/benefits-of-talking-to-someone/">reachout.org</a>, there are mental health benefits from talking to people that are close to you.</li><li>Mindfullness counts!</li>');
-    }else if(socialTotal > 5){
-      $socialComments.html('<li>Well Mr.Popular, you might want to think about cutting down on your socialising for tomorrow!</li><li>If you are struggling to get starting excercise, a good place to start is the couch to 5k running plan <a href="http://www.coolrunning.com/engine/2/2_3/181.shtml">here</a></li><li>View the subreddit <a href="www.reddit.com/r/getStudying">r/getStudying</a> for hints and tips to kick start your studying.</li>');
-    }else{
-      $socialComments.html('<li style="list-style: none;"><i class="fa fa-check fa-5x" aria-hidden="true"></i></li>');
-    }
-
-  });
 
 })
