1b60b173d36388a09be96966c6eedb6c08941a2b
John Deegan <john.deegan@hotmail.com>
2017-03-02 03:34:16 +0000

    heroku

--- views/js/profile.js
+++ views/js/profile.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   //if the user has no stored data for this date already, then show the modal, if not then go straight to the page
 
-  $.get("https://localhost:8080/tasks", function(data, status){
+  $.get("https://still-stream-97435.herokuapp.com/tasks", function(data, status){
     alert(data);
   });
 
