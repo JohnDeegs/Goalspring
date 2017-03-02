3c097df135a1b887f147e1c22d3494591b9bf8c5
John Deegan <john.deegan@hotmail.com>
2017-03-02 03:25:22 +0000

    heroku

--- views/js/profile.js
+++ views/js/profile.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   //if the user has no stored data for this date already, then show the modal, if not then go straight to the page
 
-  $.get("http://localhost:8080/tasks", function(data, status){
+  $.get("https://localhost:8080/tasks", function(data, status){
     alert(data);
   });
 
