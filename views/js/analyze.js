3b92fa4ba14223a4ab0258b2f19828d996fe6a89
John Deegan <john.deegan@hotmail.com>
2017-04-30 19:48:54 +0100

    pie chart

--- views/js/analyze.js
+++ views/js/analyze.js
@@ -15,55 +15,59 @@ $(document).ready(function() {
     var urlId = url.substr(url.length - 8);
     console.log(urlId);
 
-    var chartDataInt = [];
+    var taskData = [];
 
     $.get('/tasks/days/analyze/get/' + urlId + '', function(data, status) {
+        //Assign server data to variable;
         var obj = data;
 
+        //loop through extracted data and get the category for all data
         for (var i = 0; i < obj.length; i++) {
 
             var a = obj[i]["category"];
             var category = obj[i].category;
-            chartDataInt.push(a);
+            taskData.push(a);
         }
 
-        var finalCount;
+        console.log(taskData);
 
-        console.log(chartDataInt);
+        //Function that takes in an array and the name of the category
+        //and loops through the inputted array searching for matches to the
+        //inputted what category. If a match is found, we increment the
+        //count variable. At the end of the loop, return the value of the
+        //count variable
 
         function countInArray(array, what) {
             var count = 0;
             for (var j = 0; j < array.length; j++) {
-                if (chartDataInt[j] === what) {
+                if (taskData[j] === what) {
                     count++;
                 }
             }
-            return count
+            return count;
         }
 
-        var productiveCount = countInArray(chartDataInt, "Productive");
+        //We get the amount of times each category has been used
 
-        var arr = [];
+        var productiveCount = countInArray(taskData, "Productive");
+        var excerciseCount = countInArray(taskData, "Excercise");
+        var socialCount = countInArray(taskData, "Social");
 
-        arr.push(productiveCount);
+        //Create a new chart data variable and push the data we have
+        //collected from the database to this array.
 
-        var a = countInArray(chartDataInt, "Productive");
-        var b = countInArray(chartDataInt, "Excercise");
-        var c = countInArray(chartDataInt, "Social");
+        var barChartData = [];
 
-        var chartData = [];
+        barChartData.push(productiveCount, excerciseCount, socialCount);
 
-        chartData.push(a, b, c);
+        //console.log(chartData);
 
-        console.log(chartData);
+        //Using the chart.js framework, we can now use this data for
+        //data viz purposes
 
-        console.log(a);
+        var barCanvas = document.getElementById("barChart");
 
-        console.log(arr);
-
-        var canvas = document.getElementById("chart");
-
-        var mydata = {
+        var bar = {
             labels: ["Productive", "Excercise", "Social"],
             datasets: [{
                 label: "Tasks:",
@@ -84,22 +88,81 @@ $(document).ready(function() {
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1,
-                data: chartData,
+                data: barChartData,
             }]
         };
 
-        var myBarChart = Chart.Bar(canvas, {
-            data: mydata
+        //Using the canvas variable we assigned earlier, we can append our
+        //chart to the
+
+        var myBarChart = Chart.Bar(barCanvas, {
+            data: bar
         });
+
+        //=====PIE CHART======//
+
+        var pieData = [];
+
+        var pieNames = [];
+
+        var pieColors = [];
+
+        var times = [];
+
+        for (var i = 0; i < obj.length; i++) {
+
+            console.log(obj);
+
+            pieNames.push(obj[i]["name"])
+            var start = obj[i]["start"];
+            var end = obj[i]["end"];
+
+            var startParts = start.split("");
+            var startHour = ''+startParts[11]+''+startParts[12]+'';
+            startHour = parseInt(startHour);
+
+            var endParts = end.split("");
+            var endHour = ''+endParts[11]+''+endParts[12]+'';
+            endHour = parseInt(endHour);
+
+            if(endHour > startHour){
+              var difference = endHour - startHour;
+              console.log(difference)
+              times.push(difference);
+            }else{
+              return;
+            }
+
+            var r = Math.floor(Math.random() * 255);
+            var g = Math.floor(Math.random() * 255);
+            var b = Math.floor(Math.random() * 255);
+
+            pieColors.push("rgb("+r+","+g+","+b+")");
+        }
+
+        console.log(times);
+        console.log(pieNames);
+
+        var pieCanvas = document.getElementById("pieChart");
+
+        var mypie = {
+            labels: pieNames,
+            datasets: [{
+                data: times,
+                backgroundColor: pieColors,
+                hoverBackgroundColor: pieColors
+            }]
+        };
+
+        var myPieChart = new Chart(pieCanvas, {
+          type: 'pie',
+          data: mypie
+        });
+
+        console.log(pieData);
+
+
 
     });
 
-
-
 });
-
-/**/
-
-//chartDataInt.push("Excercise");
-
-//chartDataInt.sort();
